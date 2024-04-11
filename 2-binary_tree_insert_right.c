08a41466eb0730364584f63dd8133fcafae4a5d9
Amaara <amara.alex96@gmail.com>
2024-04-11 22:44:31 +0000

    The right node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,14 +1,29 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - deletes an entire binary tree
- * @tree: tree to delete
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: points to node to insert the right-child in
+ * @value: value of new node
+ *
+ * Return: Pointer to the newly created node
  */
-void binary_tree_delete(binary_tree_t *tree)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	if (!tree)
-		return;
-	binary_tree_delete(tree->right);
-	binary_tree_delete(tree->left);
-	free(tree);
+	binary_tree_t *new;
+
+	if (!parent)
+		return (NULL);
+
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = parent->right;
+	parent->right = new;
+	if (new->right)
+		new->right->parent = new;
+	return (new);
 }
